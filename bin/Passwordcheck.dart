2c019f5328fa23868561f524717e3e4aec340a7b
flutter-lord <richardolojede2@gmail.com>
2024-10-15 23:50:29 +0100

    commit

--- bin/Passwordcheck.dart
+++ bin/Passwordcheck.dart
@@ -9,11 +9,12 @@ class PasswordCheck {
 
     int _passwordLength = _password.length;
     int _hidePassword = _passwordLength - 4;
+    int _det = _passwordLength - 1;
 
     String i = _password.substring(0, 1);
     String j = _password.substring(1, 2);
-    String k = _password.substring(_passwordLength - 3, (_passwordLength - 2));
-    String l = _password.substring(_passwordLength - 1, _passwordLength );
+    String k = _password.substring(_det - 1, _det );
+    String l = _password.substring(_det, _det + 1);
 
     String ShowHidePassword = i + j + (('*') * _hidePassword) + k + l;
     return ShowHidePassword;
