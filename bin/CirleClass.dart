b80e9b64cbb16dae9f04a6a04c52ea5d6fe57020
flutter-lord <richardolojede2@gmail.com>
2024-10-15 23:59:48 +0100

    commit

---
+++ bin/CirleClass.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+class Circle {
+  double _radius = 0;
+
+  Circle(double radius) {
+    _radius = radius;
+  }
+
+  double get radius => _radius;
+
+  double getArea() {
+    double _area = pi * pow(_radius, 2);
+    return _area;
+  }
+
+  double getPerimeter() {
+    double _perimeter = 2 * pi * _radius;
+    return _perimeter;
+  }
+  
+
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '$_radius';
+  }
+}
---
+++ bin/TestCircleClass.dart
@@ -0,0 +1,10 @@
+import 'CirleClass.dart';
+
+void main() {
+  Circle circle = Circle(5);
+  print(circle);
+
+  print(circle.getArea());
+  
+  print(circle.getPerimeter());
+}
