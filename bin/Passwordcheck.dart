4e92e1a27d99ef177089322a179221df34e9016d
flutter-lord <richardolojede2@gmail.com>
2024-10-15 23:39:26 +0100

    commit

--- bin/Passwordcheck.dart
+++ bin/Passwordcheck.dart
@@ -4,5 +4,24 @@ class PasswordCheck {
   PasswordCheck(String password) {
     _password = password;
   }
-  
+
+  String displayPassword() {
+
+    int _passwordLength = _password.length;
+    int _hidePassword = _passwordLength - 4;
+
+    String i = _password.substring(0, 1);
+    String j = _password.substring(1, 2);
+    String k = _password.substring(_passwordLength - 3, (_passwordLength - 2));
+    String l = _password.substring(_passwordLength - 2, _passwordLength - 1);
+
+    String ShowHidePassword = i + j + (('*') * _hidePassword) + k + l;
+    return ShowHidePassword;
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return 'The password you entered is ${displayPassword()}';
+  }
 }
