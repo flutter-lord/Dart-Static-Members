c3fc1fbef3226592680b7071c55e691c5360f97f
flutter-lord <richardolojede2@gmail.com>
2024-10-17 00:54:08 +0100

    commit

--- bin/TestMath.dart
+++ bin/TestMath.dart
@@ -4,8 +4,7 @@ import 'MathClass.dart';
 
 void main() {
   Math math = Math();
-
-  print(math);
-  print(max(2, 3));
-  print(min(2, 3));
+  
+  print(Math.max(2, 3));
+  print(Math.min(2, 3));
 }
