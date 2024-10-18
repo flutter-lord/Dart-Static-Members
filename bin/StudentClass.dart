3318730d67a00a5c6a88e79dbf83b2329d8d4d2c
flutter-lord <richardolojede2@gmail.com>
2024-10-18 16:22:13 +0100

    commi

---
+++ bin/StudentClass.dart
@@ -0,0 +1,20 @@
+class StudentInfo {
+
+  String _firstName = '';
+  String _lastName = '';
+  int _grade = 0;
+
+  StudentInfo(this._firstName, this._lastName, this._grade);
+ 
+  String get firstName => _firstName;
+  String get lastName => _lastName;
+  int get grade => _grade;
+
+
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '';
+  }
+}
---
+++ bin/TestStudentClass.dart
@@ -0,0 +1,13 @@
+import 'StudentClass.dart';
+
+void main() {
+
+  StudentInfo student1 = StudentInfo('Richie', 'Olojede', 85);
+  StudentInfo student2 = StudentInfo('Christopher', 'Olojede', 97);
+
+  print(student1);
+  print(student2);
+
+
+
+}
