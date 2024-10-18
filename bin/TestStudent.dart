7ee5c7e5b69732bc1bfb46bb1b79f519683604dd
flutter-lord <richardolojede2@gmail.com>
2024-10-18 17:46:44 +0100

    commit

--- bin/TestStudent.dart
+++ bin/TestStudent.dart
@@ -5,16 +5,20 @@ void main() {
   final student1 = Student(1, 'Ade', 15, 'Science');
   print('The number of student here is ${Student.numberOfStudent}');
   student1.canStudentApplyForWAEC();
+  print('');
 
   final student2 = Student(2, 'Ayo', 23, 'Education');
   print('The number of student here is ${Student.numberOfStudent}');
   student2.canStudentApplyForWAEC();
+  print('');
 
-   final student3 = Student(3, 'Afo', 25, 'Education');
+  final student3 = Student(3, 'Afo', 25, 'Education');
   print('The number of student here is ${Student.numberOfStudent}');
   student3.canStudentApplyForWAEC();
+  print('');
 
   final student4 = Student.defaultStudent(); 
+  print(student4);
 
 
 }
