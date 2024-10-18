39d70cabdc2ffbf20d3166cd7f307aeed596a35f
flutter-lord <richardolojede2@gmail.com>
2024-10-18 16:34:09 +0100

    commit

--- bin/TestStudentClass.dart
+++ bin/TestStudentClass.dart
@@ -2,11 +2,11 @@ import 'StudentClass.dart';
 
 void main() {
 
-  StudentInfo student5 = StudentInfo('Oladimeji', 'Olojede', 100);
-  StudentInfo student4 = StudentInfo('Olawunmi', 'Olojede', 94);
-  StudentInfo student2 = StudentInfo('Christopher', 'Olojede', 97);
-  StudentInfo student1 = StudentInfo('Richie', 'Olojede', 85);
-  StudentInfo student3 = StudentInfo('Bosun', 'Olojede', 88);
+  StudentInfo student1 = StudentInfo('Oladimeji', 'Olojede', 100);
+  StudentInfo student2 = StudentInfo('Olawunmi', 'Olojede', 94);
+  StudentInfo student3 = StudentInfo('Christopher', 'Olojede', 97);
+  StudentInfo student4 = StudentInfo('Richie', 'Olojede', 85);
+  StudentInfo student5 = StudentInfo('Bosun', 'Olojede', 88);
 
 
   print(student1);
@@ -14,7 +14,6 @@ void main() {
   print(student3);
   print(student4);
   print(student5);
-
 
 
 }
