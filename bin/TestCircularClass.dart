079a40b47a3aff7c5b0a84d8c4fac13786e90a9f
flutter-lord <richardolojede2@gmail.com>
2024-10-18 16:56:01 +0100

    commit

--- bin/TestCircularClass.dart
+++ bin/TestCircularClass.dart
@@ -5,8 +5,8 @@ void main() {
   Circular circle2 = Circular(10);
   Circular circle3 = Circular(20);
 
-  print('The area of circle1 is ${circle1.getArea()}\nThe perimeter of circle1 is ${circle1.getPerimeter()}');
-  print('The area of circle2 is ${circle2.getArea()}\nThe perimeter of circle2 is ${circle2.getPerimeter()}');
-  print('The area of circle3 is ${circle3.getArea()}\nThe perimeter of circle3 is ${circle3.getPerimeter()}');
+  print('The area of circle1 is ${circle1.getArea()}\nThe perimeter of circle1 is ${circle1.getPerimeter()}\n');
+  print('The area of circle2 is ${circle2.getArea()}\nThe perimeter of circle2 is ${circle2.getPerimeter()}\n');
+  print('The area of circle3 is ${circle3.getArea()}\nThe perimeter of circle3 is ${circle3.getPerimeter()}\n');
   
 }
