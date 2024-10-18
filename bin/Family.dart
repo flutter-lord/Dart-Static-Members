11085aad445ba96bc138a564832ee6a46113be13
flutter-lord <richardolojede2@gmail.com>
2024-10-18 16:41:46 +0100

    commit

---
+++ bin/Family.dart
@@ -0,0 +1,13 @@
+void main() {
+  addSurname('Oladimeji', 100);
+  addSurname('Olawunmi', 97);
+  addSurname('Olamide', 94);
+  addSurname('Olanrewaju', 91);
+  addSurname('Olabosun', 88);
+
+  
+}
+
+void addSurname(String _firstName, int _grade) {
+  print('$_firstName Olojede has a grade of $_grade');
+}
