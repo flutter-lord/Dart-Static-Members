ad262457dc32eed50edc3342aa1475cd65aa5442
flutter-lord <richardolojede2@gmail.com>
2024-10-19 15:11:04 +0100

    commit

--- bin/TestShortcut2.dart
+++ bin/TestShortcut2.dart
@@ -1,7 +1,14 @@
-import '../lib/Shortcut.dart';
-//import 'Shortcut2.dart';
+import 'dart:io';
 
 void main() {
-  print('${max(2, 3)}\n${min(2, 3)}');
+  stdout.write('Enter your number: ');
+  num _number = int.parse(stdin.readLineSync()!); 
+  
+  print(positiveNumber(_number));
 
+}
+
+bool positiveNumber(_number) {
+  bool _positive = !(_number.isNegative);
+  return _positive;
 }
