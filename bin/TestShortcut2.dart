246da8b0e1bd8ed3abcfa0bb82d912c34e069e9d
flutter-lord <richardolojede2@gmail.com>
2024-10-19 15:15:35 +0100

    commit

--- bin/TestShortcut2.dart
+++ bin/TestShortcut2.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main() {
   stdout.write('Enter your number: ');
-  num? _number = int.parse(stdin.readLineSync()!); 
+  dynamic _number = int.parse(stdin.readLineSync()!); 
 
   print(positiveNumber(_number));
 
