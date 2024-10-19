76d8c37306eb450ae9bd8ce29a8f877aee93fb82
flutter-lord <richardolojede2@gmail.com>
2024-10-19 22:15:13 +0100

    commit

--- bin/TestName.dart
+++ bin/TestName.dart
@@ -1,7 +1,15 @@
+import 'dart:io';
+
 import 'NameClass.dart';
 
 void main() {
-  Name name = Name('Lanre', 'Olojede');
+  stdout.write('Enter your name: ');
+  String _givenName = stdin.readLineSync()!;
+
+  stdout.write('Enter your surname if you have, if not leave if empty: ');
+  String _surname = stdin.readLineSync()!;
+
+  Name name = Name(_givenName, _surname);
 
   print(name);
 }
