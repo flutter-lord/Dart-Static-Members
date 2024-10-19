eaccb87bcfe7d9faa62cbb210fee3c6ba3be347a
flutter-lord <richardolojede2@gmail.com>
2024-10-19 10:49:45 +0100

    commit

---
+++ bin/Shortcut2.dart
@@ -0,0 +1,9 @@
+class Math {
+  num max(num a, num b) {
+    return(a > b) ? a : b; 
+  }
+
+  num min(num a, num b) {
+    return(a < b) ? a : b;
+  }
+}
---
+++ bin/TestShortcut2.dart
@@ -0,0 +1,7 @@
+import '../lib/Shortcut.dart';
+//import 'Shortcut2.dart';
+
+void main() {
+  print('${max(2, 3)}\n${min(2, 3)}');
+
+}
