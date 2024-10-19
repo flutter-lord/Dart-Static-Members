f294070e503e41a393d218d6df78cd1b179bc263
flutter-lord <richardolojede2@gmail.com>
2024-10-19 22:08:51 +0100

    commit

---
+++ bin/NameClass.dart
@@ -0,0 +1,35 @@
+class Name {
+  String _givenName = '';
+  String? _surname = '';
+
+
+  Name(this._givenName, [this._surname]);
+  
+
+  bool surnameisFirst () {
+    bool _checkSurname =  _surname!.startsWith(_surname!);
+    return _checkSurname;
+  }
+
+  String nameOrder() {
+    return'${_surname! + '' + _givenName} ';
+  }
+  String output() {
+    if (_surname == null) {
+      return _givenName;
+    }
+
+    else {
+      return '${nameOrder()}';
+    }
+  }
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return '${output()}';
+  }
+
+  
+
+}
---
+++ bin/TestName.dart
@@ -0,0 +1,7 @@
+import 'NameClass.dart';
+
+void main() {
+  Name name = Name('Lanre', 'Olojede');
+
+  print(name);
+}
