8f4291a835f4a116b3b82490246020f0e5d81557
flutter-lord <richardolojede2@gmail.com>
2024-10-20 22:23:19 +0100

    commit

--- bin/MyUserClass.dart
+++ bin/MyUserClass.dart
@@ -6,11 +6,11 @@ class MyUser {
   String? hubby = '';
 
   MyUser(this.name, this.id, this.department, [this.hubby]);
-  
+
 
   @override
   String toString() {
     // TODO: implement toString
-    return 'User(name: $name, id: $id, department: $department)';
+    return 'User(name: $name, id: $id, department: $department, ${['Hubby: $hubby']})';
   }
 }
