f3438a16914fcd13fcf6e23c89b258dd72b04315
flutter-lord <richardolojede2@gmail.com>
2024-10-20 15:58:10 +0100

    commit

--- bin/TestShortcut2.dart
+++ bin/TestShortcut2.dart
@@ -2,19 +2,18 @@ import 'dart:io';
 
 void main() {
   stdout.write('Enter your number: ');
-  int _number = int.parse(stdin.readLineSync()!); 
+  int? _number = int.parse(stdin.readLineSync()!); 
 
-  String? _name;
-  _name = 'ray';
+  if (_number == true) {
+    print(null);
+  }
 
+  else {
+    print(!(_number.isNegative));
+  }
 
-  print(positiveNumber(_number));
-
-  print(_name.length);
+  // print(positiveNumber(_number));
 
 }
 
-bool positiveNumber(dynamic _number) {
-  bool _positive = !(_number.isNegative);
-  return _positive;
-}
+// bool positiveNumber(dynamic _number) {
