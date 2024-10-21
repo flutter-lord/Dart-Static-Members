bf18547bfeace85ac385228a48520fd12d8fac7a
flutter-lord <richardolojede2@gmail.com>
2024-10-21 15:05:43 +0100

    commit

--- bin/ForLoop.dart
+++ bin/ForLoop.dart
@@ -1,8 +1,8 @@
 void main() {
-  
+
   for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= 5; j++) {
-      for (int k = 1; k <= 5; j++) {
+      for (int k = 1; k <= 5; k++) {
         print('i = $i, j = $j, k = $k');
       }
     }
