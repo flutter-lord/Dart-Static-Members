0bb14bcd2dc95842b612e1a215ace0eaa586436f
flutter-lord <richardolojede2@gmail.com>
2024-10-24 13:09:34 +0100

    commit

--- bin/SecurityClass.dart
+++ bin/SecurityClass.dart
@@ -116,9 +116,7 @@ class Security {
      return 'Try entering 8 - 16 characters';
    }
 
-   else {
     return 'Your password ${hidePassword()} that you entered is not valid';
-   }
   }
 
   @override
